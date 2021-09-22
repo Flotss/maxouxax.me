9fe526433af845bea1ba54d1106028c788fd1f45
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2021-09-22 21:58:40 +0200

    Reintroducing GROUBOX, UpperCaseGenerator, and added a 404 page

---
+++ src/views/MEJRI.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="mejri">
+      <div class="title">
+		<h1>aAaAaaAaAaaaAAaaaAaaaa</h1>
+	</div>
+	<p>
+		<q><i>If you are not on the web, you will have problems accessing services.</i></q>Tim Berners-Lee.
+	</p>
+	<p>
+		<strong><i>HyperText Markup Language</i>(HTML)</strong>,
+		ou <i>langage de balisage hypertexte</i>, est le
+		langage au cœur de presque tout contenu Web.
+		La majorité de ce que vous voyez dans votre navigateur est
+		décrit en utilisant HTML.
+		Plus précisément, HTML est le langage qui décrit la
+		structure et la sémantique du contenu d'un document Web.
+		Le contenu au sein d'un document Web est balisé par des
+		éléments HTML.
+	</p>
+	<p>
+		HTML est un standard international dont la spécification est
+		maintenue par le <em>World Wide Web Consortium</em> et le
+		WHATWG.
+		Il est considéré comme un <q>standard vivant</q> et est
+		techniquement toujours en évolution.
+		La version toujours-à-jour de la spécification HTML est
+		appelée <strong>HTML5</strong>.
+	</p>
+	<p>
+		HTML5 <strong>ne</strong> décrit <strong>pas</strong> le
+		style et la mise en forme du contenu, mais seulement le
+		contenu lui-même et son <strong>sens</strong>. Le style et
+		la mise en page sont définis et contrôlés par les
+		<em>Cascading Style Sheets</em> (CSS).
+	</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap');
+
+*{
+    margin: 0px;
+    padding: 0px;
+    font-family: 'Cabin', sans-serif;
+}
+.mejri{
+    background-image: url("https://c.tenor.com/wKAogI7UDyoAAAAd/mejri-mohammed.gif");
+    animation: background 3s alternate infinite;
+    background-repeat: repeat;
+    background-position: 50% 50%;
+    width: 100%;
+    height: 100%;
+}
+.title{
+    width: 100%;
+    height: 130px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+h1{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    animation: title 1s alternate-reverse infinite;
+}
+h1::after, h1::before{
+    content: '';
+    position: relative;
+    background-color: cyan;
+    width: 20px;
+    height: 20px;
+    border-radius: 50px;
+    animation: animate 1.2s backwards infinite;
+}
+h1::after{
+    left: 25px;
+}
+h1::before{
+    right: 25px;
+}
+@keyframes animate{
+    0%{
+        transform: scale(1, 1);
+        opacity: 1;
+    }
+    100%{
+        transform: scale(5, 5);
+        opacity: 0;
+    }
+}
+@keyframes title{
+    0%{
+        transform: skew(20deg, 20deg);
+    }
+    100%{
+        transform: skew(-20deg, -20deg);
+    }
+}
+@keyframes background{
+    0%{
+        background-size: 200px 200px;
+    }
+    100%{
+        background-size: 500px 2000px;
+    }
+}
+</style>
---
+++ src/views/UpperCaseGenerator.vue
@@ -0,0 +1,294 @@
+<template>
+	<div>
+		<header>
+            <div class="title">
+                <p id="title">UpperCaseGenerator</p>
+            </div>
+            <div class="divider"></div>
+        </header>
+        <main>
+            <div class="convert">
+                <div class="input">
+                    <p>Texte à modifier</p>
+                    <textarea class="textarea" id="input" name="Texte à modifier" maxlength="900"></textarea>
+                </div>
+                <div class="output">
+                    <p>Résultat</p>
+                    <textarea class="resultarea" id="output" name="Résultat" readonly="readonly"></textarea>
+                </div>
+                <div class="clipboard">
+                    <button onclick="copyToClipboard()">Copier</button>
+                    <br>
+                    <i id="clip" class="fas fa-check fa-5x"></i>
+                </div>
+            </div>
+            <div class="divider"></div>
+            <div class="whycontainer">
+                <div class="why">
+                    <h4>Pourquoi ce site ?</h4>
+                    <p class="about">UpperCaseGenerator est un mini-service vous permettant de transformer vos phrases en quelques instants dans une forme rEsSeMbLaNt à Ça :)</p>
+                    <p class="about">Pour utiliser ce mini-service, il vous suffit d'écrire votre texte original dans l'emplacement prévu à cet effet, et le site se chargera de tout. Vous pourrez retrouver le résultat dans l'emplacement juste en dessous.</p>
+                </div>
+            </div>
+        </main>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+var schedule = setInterval(updateData, 500);
+var displayedText = "";
+
+function updateData(){
+    var text = document.getElementById("input").value;
+    if(!(text == displayedText)){
+        var str = "";
+        for (var i = 0; i < text.length; i++) {
+            (i % 2) ? str = str+text.charAt(i).toUpperCase() : str = str+text.charAt(i).toLowerCase();
+        }
+        displayedText = str;
+        document.getElementById("output").value = displayedText;
+    }
+}
+
+function copyToClipboard(){
+  var copyText = document.getElementById("output");
+  var check = document.getElementById("clip");
+
+  copyText.select();
+
+  document.execCommand("copy");
+  check.style.transform = 'scale(1,1)';
+  setTimeout(() => {
+      check.style.transform = 'scale(0,0)'
+  }, 1500);
+}
+</script>
+
+<style scoped>
+body{
+    background-color: #070707;
+}
+.title{
+    color: white;
+    text-align: center;
+    font-family: 'Roboto', sans-serif;
+    margin-bottom: 80px;
+    transition: 1.4s cubic-bezier(0.86, 0, 0.07, 1);
+}
+#title{
+    color: white;
+    text-align: center;
+    font-family: 'Roboto', sans-serif;
+    font-size: 8vw;
+    transition: 1.4s cubic-bezier(0.86, 0, 0.07, 1);
+}
+#title:hover{
+    transform: scale(0.95, 0.95);
+    transition: 1.4s cubic-bezier(0.86, 0, 0.07, 1);    
+}
+.made{
+    text-align: center;
+    color: crimson;
+    text-shadow: 2px 2px 3px black;
+    font-size: 5vw;
+    transition: 1.4s cubic-bezier(0.86, 0, 0.07, 1);    
+}
+.version{
+    text-align: center;
+    color: white;
+    font-size: 5vw;
+    transition: 1.4s cubic-bezier(0.86, 0, 0.07, 1);
+}
+footer{
+    font-family: 'Roboto', sans-serif;
+    padding: 10px;
+    position: relative;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-image: linear-gradient(#070707 , black);
+    transition: 1.4s cubic-bezier(0.86, 0, 0.07, 1);
+}
+footer:hover{
+    box-shadow: 0px 0px 100px white;
+    transition: 1.4s cubic-bezier(0.86, 0, 0.07, 1);
+}
+html{
+    position:relative; 
+    min-height: 100%;
+}
+html,body{
+    margin:0;
+    padding:0;
+}
+main{
+    animation-name: appearing;
+    animation-duration: 2s;
+}
+header{
+    animation-name: appearing;
+    animation-duration: 2s;
+}
+p{
+    color: white;
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.2rem;
+    text-align: center;
+}
+textarea{
+    margin-left: auto;
+    margin-right: auto;
+    width: 70vw;
+    height: 250px;
+    background-color: #020202;
+    color: white;
+    border: 1px solid #95a5a6;
+    max-width: 600px;
+    max-height: 400px;
+    font-size: 1rem;
+    font-family: 'Roboto', sans-serif;
+    transition: 0.4s cubic-bezier(0.175, 0.885, 0.32, 1);
+}
+textarea:hover{
+    transition: 0.4s cubic-bezier(0.175, 0.885, 0.32, 1);
+}
+.convert{
+    padding: 50px;
+    margin: 1vw;
+    text-align: center;
+}
+.divider{
+    height: 2px;
+    width: 70vw;
+    background-color: white;
+    margin-left:auto;
+    margin-right:auto;
+}
+.why{
+    width: 50vw;
+    text-align: center;
+    margin-left:auto;
+    margin-right:auto;
+    padding: 50px;
+    border: 0px solid #070707;
+    transition: 1.4s cubic-bezier(0.86, 0, 0.07, 1);
+    box-shadow: 0px 0px 0px #070707;
+    animation: wow 10s infinite;
+}
+.whycontainer{
+    text-align: center;
+    margin-left:auto;
+    margin-right:auto;
+    transition: 1s;
+    padding: 1px;
+    margin-top: 80px;
+    margin-bottom: 80px;
+}
+.about{
+    color: white;
+    font-family: 'Roboto', sans-serif;
+    font-size: 4.5vw;
+    text-align: center;
+    transition: 1.4s cubic-bezier(0.86, 0, 0.07, 1);
+}
+h4{
+    color: white;
+    font-size: 7vw;
+    font-family: 'Roboto', sans-serif;
+    text-align: center;
+    transition: 1.4s cubic-bezier(0.86, 0, 0.07, 1);
+}
+#clip{
+    transform: scale(0,0);
+    color: #2ecc71;
+    transition: 1.4s cubic-bezier(0.86, 0, 0.07, 1);
+}
+button{
+    padding: 10px;
+    margin: 2vw;
+    margin-top: 40px;
+    font-family: 'Roboto', sans-serif;
+    font-size: 2rem;
+
+    border: 1px solid #1976D2;
+    box-shadow: 2px 2px 5px black;
+    height: 4.5rem;
+    color: #1976D2;
+    background: none transparent;
+    text-transform: uppercase;
+    cursor: pointer;
+    transition: 0.8s;
+    position: relative;
+    overflow: hidden;
+    border-radius: 50px
+}
+
+button:hover{
+    color: #fff;
+    transform: scale(1.3,1.3);
+    transition: 0.8s;
+  }
+@keyframes appearing{
+    0%{
+        opacity: 0;
+    }
+    100%{
+        opacity: 100;
+    }
+}
+@keyframes wow{
+    0%{
+        text-align: center;
+        margin-left:auto;
+        margin-right:auto;
+        transition: 1s;
+        padding: 50px;
+        border: 0px solid #070707;
+        transition: 1.4s cubic-bezier(0.86, 0, 0.07, 1);
+        box-shadow: 0px 0px 0px #070707;
+    }
+    15%{
+        text-align: center;
+        margin-left:auto;
+        margin-right:auto;
+        padding: 50px;
+        transform: scale(0.95, 0.95);
+        border: 4px solid white;
+        transition: 1.4s cubic-bezier(0.86, 0, 0.07, 1);
+        box-shadow: 3px 3px 8px black;
+    }
+    30%{
+        text-align: center;
+        margin-left:auto;
+        margin-right:auto;
+        transition: 1s;
+        padding: 50px;
+        border: 0px solid #070707;
+        transition: 1.4s cubic-bezier(0.86, 0, 0.07, 1);
+        box-shadow: 0px 0px 0px #070707;
+    }
+}
+@media screen and (min-width: 1000px) {
+    h4 {
+       font-size: 4rem;
+    }
+    .about{
+        font-size: 2rem;
+    }
+    .made{
+        font-size: 1.3rem;
+    }
+    .version{
+        font-size: 1rem;
+    }
+    #title{
+        font-size: 4rem;
+    }
+    .divider{
+        width: 700px;
+    }
+  }
+</style>
