6ffa71381fc4ad6e1590a0d590ea1b8c46f5e321
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2022-05-07 15:36:17 +0200

    Added button row at the bottom of the footer to include status page link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,12 @@
 <template>
-  <v-footer
-    inset
-    width="auto"
-    padless
-    elevation="20"
-  >
-    <v-card
-      flat
-      tile
-      class="lighten-1 text-center"
-    >
+  <v-footer inset width="auto" padless elevation="20">
+    <v-card flat tile class="lighten-1 text-center">
       <v-card-text>
-        <a rel="noreferrer" v-for="icon in icons" :key="icon.icon" :href="icon.link" target="_blank">
         <v-btn
+          v-for="icon in icons"
+          :key="icon.icon"
+          :href="icon.link"
+          target="_blank"
           class="mx-4"
           icon
         >
@@ -20,50 +14,94 @@
             {{ icon.icon }}
           </v-icon>
         </v-btn>
-        </a>
       </v-card-text>
 
       <v-card-text class="pt-0">
         Étudiant en première année • BUT Informatique
       </v-card-text>
 
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="link in links"
+          :key="link.name"
+          :href="link.url"
+          :to="link.to"
+          :target="link.to ? '' : '_blank'"
+          text
+          rounded
+          class="link mx-1 my-2"
+        >
+          {{ link.name }}
+        </v-btn>
+      </v-row>
+
       <v-divider></v-divider>
 
       <v-card-text class="copyright">
-        <v-icon size="14px">mdi-copyright</v-icon>2021 - {{ new Date().getFullYear() }} — <strong>MAXOUXAX</strong>
+        <v-icon size="14px">mdi-copyright</v-icon>2021 -
+        {{ new Date().getFullYear() }} — <strong>MAXOUXAX</strong>
       </v-card-text>
     </v-card>
   </v-footer>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      icons: [
-        {icon: 'mdi-twitter', link: 'https://twitter.com/MAXOUXAX/', name: 'Twitter'},
-        {icon: 'mdi-youtube', link: 'https://youtube.com/MAXOUXAX', name: 'YouTube'},
-        {icon: 'mdi-github', link: 'https://github.com/MAXOUXAX', name: 'GitHub'},
-      ],
-    }),
-  }
+export default {
+  data: () => ({
+    icons: [
+      {
+        icon: "mdi-twitter",
+        link: "https://twitter.com/MAXOUXAX/",
+        name: "Twitter",
+      },
+      {
+        icon: "mdi-youtube",
+        link: "https://youtube.com/MAXOUXAX",
+        name: "YouTube",
+      },
+      {
+        icon: "mdi-github",
+        link: "https://github.com/MAXOUXAX",
+        name: "GitHub",
+      },
+    ],
+    links: [
+      {
+        name: "Accueil",
+        to: "/",
+      },
+      {
+        name: "Projets",
+        to: "/projects",
+      },
+      {
+        name: "État des services",
+        url: "https://status.maxouxax.me/",
+      },
+    ],
+  }),
+};
 </script>
 
 <style>
-footer{
-    width: 100%;
+footer {
+  width: 100%;
 }
-footer .copyright{
-    user-select: none;
-    -webkit-user-select: none;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
+footer .copyright {
+  user-select: none;
+  -webkit-user-select: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
 }
-footer .copyright *{
-    margin: 0px 3px;
+footer .copyright * {
+  margin: 0px 3px;
 }
-footer div{
-    width: 100%;
+footer div {
+  width: 100%;
+}
+footer .link::before {
+  opacity: 0 !important;
 }
 </style>
