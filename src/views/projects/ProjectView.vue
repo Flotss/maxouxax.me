0b02e9398d3e4276cba919d5a3f6bab66c3c0d52
maxouxax <24844231+MAXOUXAX@users.noreply.github.com>
2022-05-23 09:35:43 +0200

    Fixed buttons overflowing

--- src/views/projects/ProjectView.vue
+++ src/views/projects/ProjectView.vue
@@ -3,34 +3,34 @@
     <page-title v-if="!loading" class="mb-16">{{ repository.name }}</page-title>
     <div class="d-flex align-center justify-center" v-if="networkError">
       <v-alert
-          border="left"
-          icon="mdi-alert-circle"
-          transition="slide-y-transition"
-          elevation="4"
-          type="error"
-          :width="vAlertWidth"
+        border="left"
+        icon="mdi-alert-circle"
+        transition="slide-y-transition"
+        elevation="4"
+        type="error"
+        :width="vAlertWidth"
       >
-        Une erreur est survenue lors du chargement des informations du projet. Veuillez
-        vérifier votre connexion Internet ou réessayer plus tard.
+        Une erreur est survenue lors du chargement des informations du projet.
+        Veuillez vérifier votre connexion Internet ou réessayer plus tard.
       </v-alert>
     </div>
     <div class="not-found-error" v-if="notFound">
       <v-alert
-          border="left"
-          icon="mdi-alert-circle"
-          transition="slide-y-transition"
-          elevation="4"
-          type="error"
-          :width="vAlertWidth"
+        border="left"
+        icon="mdi-alert-circle"
+        transition="slide-y-transition"
+        elevation="4"
+        type="error"
+        :width="vAlertWidth"
       >
         Ce projet n'existe pas. Veuillez retourner à la liste des projets.
       </v-alert>
     </div>
     <v-skeleton-loader v-if="loading && !networkError && !notFound">
       <v-skeleton-loader
-          class="mx-auto d-flex justify-center mb-16"
-          max-width="800px"
-          type="heading"
+        class="mx-auto d-flex justify-center mb-16"
+        max-width="800px"
+        type="heading"
       ></v-skeleton-loader>
       <v-row align="center" justify="center" class="mx-0">
         <v-sheet :width="vSkeletonWidth" class="mx-5 my-4">
@@ -39,8 +39,8 @@
       </v-row>
     </v-skeleton-loader>
     <div
-        v-if="!loading"
-        class="project d-flex justify-center align-center align-content-center"
+      v-if="!loading"
+      class="project d-flex justify-center align-center align-content-center"
     >
       <v-container full-width>
         <v-row class="mb-6 justify-center" no-gutters>
@@ -63,23 +63,29 @@
           <v-col>
             <v-card class="ma-5">
               <v-card-title primary-title> Actions</v-card-title>
-              <v-card-actions class="pa-4">
-                <v-btn
-                    v-for="action in getActions"
-                    :key="action.text"
+              <v-card-actions class="pa-4 flex-wrap">
+                <span v-for="action in getActions" :key="action.text">
+                  <v-btn
                     :color="action.color"
                     target="_blank"
                     rel="noopener"
                     :href="action.href"
                     @click="action.onClick($data)"
-                >
-                  <v-icon left> {{ action.icon }}</v-icon>
-                  {{ action.text }}
-                </v-btn>
+                    class="ma-0 mr-1 mt-1 d-inline-block"
+                  >
+                    <v-icon left> {{ action.icon }}</v-icon>
+                    <span style="white-space: normal">{{ action.text }}</span>
+                  </v-btn>
+                </span>
                 <v-snackbar elevation="12" v-model="copied" timeout="3000">
                   Adresse copiée !
                   <template v-slot:action="{ attrs }">
-                    <v-btn color="info" text v-bind="attrs" @click="copied = false">
+                    <v-btn
+                      color="info"
+                      text
+                      v-bind="attrs"
+                      @click="copied = false"
+                    >
                       Fermer
                     </v-btn>
                   </template>
@@ -87,19 +93,20 @@
               </v-card-actions>
               <v-divider></v-divider>
               <v-card-text>
-                <v-chip-group column>
+                <v-col>
                   <v-chip
-                      v-for="chip in getChips"
-                      :key="chip.text"
-                      :color="chip.color"
-                      text-color="white"
+                    v-for="chip in getChips"
+                    :key="chip.text"
+                    :color="chip.color"
+                    text-color="white"
+                    class="ml-1 mt-2 mb-1"
                   >
                     <v-icon left>
                       {{ chip.icon }}
                     </v-icon>
                     {{ chip.text }}
                   </v-chip>
-                </v-chip-group>
+                </v-col>
               </v-card-text>
             </v-card>
           </v-col>
@@ -112,7 +119,7 @@
 <script>
 import GitHubRepository from "@/components/GitHubRepository.vue";
 import PageTitle from "@/components/PageTitle.vue";
-import {marked} from "marked";
+import { marked } from "marked";
 
 export default {
   name: "project-view",
@@ -126,74 +133,80 @@ export default {
   methods: {
     getRepository: async function () {
       await fetch("https://api.github.com/repos/MAXOUXAX/" + this.projectName)
-          .then((response) => {
-            if (response.status === 403) throw new Error('Rate limit exceeded');
-            return response.json()
-          })
-          .then((data) => {
-            if (data.message === "Not Found") {
-              this.notFound = true;
-            } else {
-              this.repository = {
-                name: data.name,
-                url: data.html_url,
-                description: data.description,
-                language: data.language,
-                license: data.license,
-                stars: data.stargazers_count,
-                archived: data.archived,
-                pushed: data.pushed_at,
-              };
-            }
-          })
-          .catch((error) => {
-            this.networkError = true;
-          });
+        .then((response) => {
+          if (response.status === 403) throw new Error("Rate limit exceeded");
+          return response.json();
+        })
+        .then((data) => {
+          if (data.message === "Not Found") {
+            this.notFound = true;
+          } else {
+            this.repository = {
+              name: data.name,
+              url: data.html_url,
+              description: data.description,
+              language: data.language,
+              license: data.license,
+              stars: data.stargazers_count,
+              archived: data.archived,
+              pushed: data.pushed_at,
+            };
+          }
+        })
+        .catch((error) => {
+          this.networkError = true;
+        });
       let mainBranchName;
       await fetch(
-          "https://api.github.com/repos/MAXOUXAX/" +
+        "https://api.github.com/repos/MAXOUXAX/" +
           this.projectName +
           "/branches"
       )
-          .then((response) => {
-            if (response.status === 403) throw new Error('Rate limit exceeded');
-            return response.json()
-          })
-          .then((data) => {
-            return data.filter((branch) => {
-              if (branch.name === "master" || branch.name === "main") {
-                mainBranchName = branch.name;
-              }
-            });
-          })
-          .catch((error) => {
-            this.networkError = true;
+        .then((response) => {
+          if (response.status === 403) throw new Error("Rate limit exceeded");
+          return response.json();
+        })
+        .then((data) => {
+          return data.filter((branch) => {
+            if (branch.name === "master" || branch.name === "main") {
+              mainBranchName = branch.name;
+            }
           });
+        })
+        .catch((error) => {
+          this.networkError = true;
+        });
       await fetch(
-          "https://api.github.com/repos/MAXOUXAX/" +
+        "https://api.github.com/repos/MAXOUXAX/" +
           this.repository.name +
           "/branches/" +
           mainBranchName
       )
-          .then((response) => {
-            if (response.status === 403) throw new Error('Rate limit exceeded');
-            return response.json()
-          })
-          .then((data) => {
-            this.repository.lastCommit = data.commit.commit.author.date;
-          })
-          .catch((error) => {
-            this.networkError = true;
-          });
-      this.repository.readme = await fetch("https://raw.githubusercontent.com/MAXOUXAX/" + this.projectName + "/" + mainBranchName + "/README.md")
-          .then((response) => {
-            if (response.status === 403) throw new Error('Rate limit exceeded');
-            return response.text()
-          })
-          .catch((error) => {
-            this.networkError = true;
-            return "";
-          });
+        .then((response) => {
+          if (response.status === 403) throw new Error("Rate limit exceeded");
+          return response.json();
+        })
+        .then((data) => {
+          this.repository.lastCommit = data.commit.commit.author.date;
+        })
+        .catch((error) => {
+          this.networkError = true;
+        });
+      this.repository.readme = await fetch(
+        "https://raw.githubusercontent.com/MAXOUXAX/" +
+          this.projectName +
+          "/" +
+          mainBranchName +
+          "/README.md"
+      )
+        .then((response) => {
+          if (response.status === 403) throw new Error("Rate limit exceeded");
+          return response.text();
+        })
+        .catch((error) => {
+          this.networkError = true;
+          return "";
+        });
     },
   },
   async mounted() {
@@ -228,7 +241,8 @@ export default {
 
       let date = new Date(this.repository.pushed);
       let repositoryHealthColor;
-      let latestCommitTimeDeltaInDays = (new Date().getTime() - date.getTime()) / (1000 * 60 * 60 * 24);
+      let latestCommitTimeDeltaInDays =
+        (new Date().getTime() - date.getTime()) / (1000 * 60 * 60 * 24);
       if (latestCommitTimeDeltaInDays < 30) {
         repositoryHealthColor = "success";
       } else if (latestCommitTimeDeltaInDays < 90) {
@@ -241,28 +255,30 @@ export default {
       return [
         {
           text: this.repository.stars,
-          icon: 'mdi-star',
-          color: 'primary',
+          icon: "mdi-star",
+          color: "primary",
         },
         {
           text: date.toLocaleDateString(undefined, {
             year: "numeric",
-            month: "long"
+            month: "long",
           }),
-          icon: 'mdi-update',
+          icon: "mdi-update",
           color: repositoryHealthColor,
         },
         {
           text: this.repository.language,
-          icon: 'mdi-language',
-          color: 'secondary',
+          icon: "mdi-language",
+          color: "secondary",
         },
         {
-          text: this.repository.license ? this.repository.license.name : "Aucune licence",
-          icon: 'mdi-license',
-          color: 'secondary',
+          text: this.repository.license
+            ? this.repository.license.name
+            : "Aucune licence",
+          icon: "mdi-license",
+          color: "secondary",
         },
-      ]
+      ];
     },
     getActions() {
       if (this.repository == null) {
@@ -272,34 +288,36 @@ export default {
       return [
         {
           text: "Voir sur GitHub",
-          icon: 'mdi-github',
-          color: 'primary',
+          icon: "mdi-github",
+          color: "primary",
           href: this.repository.url,
-          onClick(){
+          onClick() {
             //TODO: Double check that
-          }
+          },
         },
         {
           text: "Clone (SSH)",
           icon: "mdi-powershell",
           color: "accent",
           href: null,
-          onClick(data){
-            navigator.clipboard.writeText("git@github.com:MAXOUXAX/" + data.repository.name + ".git");
+          onClick(data) {
+            navigator.clipboard.writeText(
+              "git@github.com:MAXOUXAX/" + data.repository.name + ".git"
+            );
             data.copied = true;
-          }
+          },
         },
         {
           text: "Clone (HTTPS)",
           icon: "mdi-lock",
           color: "accent",
           href: null,
-          onClick(data){
+          onClick(data) {
             navigator.clipboard.writeText(data.repository.url + ".git");
             data.copied = true;
-          }
+          },
         },
-      ]
+      ];
     },
     compileMarkdown() {
       if (this.repository == null) {
